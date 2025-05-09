985bf1ca102d09e532b2f490209a54b4258dc6f4
OopyDoopy <jong833@gmail.com>
2025-05-09 14:59:59 -0400

    feat(metro): beta1 - initial release

--- src/games/metro/common.hlsl
+++ src/games/metro/common.hlsl
@@ -1,18 +1,24 @@
 #include "./shared.h"
 
 float3 CustomTonemap(float3 untonemapped, float3 tonemapped_bt709) {
+  untonemapped = renodx::color::srgb::DecodeSafe(untonemapped);
+  tonemapped_bt709 = renodx::color::srgb::DecodeSafe(tonemapped_bt709);
   float3 outputColor;
     if (RENODX_TONE_MAP_TYPE == 0.f) {
-      outputColor = renodx::color::srgb::DecodeSafe(tonemapped_bt709);
+      outputColor = saturate(tonemapped_bt709);
     } else {
-      outputColor = renodx::draw::ToneMapPass(
-          renodx::color::srgb::DecodeSafe(untonemapped),
-          renodx::color::srgb::DecodeSafe(tonemapped_bt709));
+      outputColor = renodx::draw::ToneMapPass(untonemapped, tonemapped_bt709);
     }
-//   if (RENODX_TONE_MAP_TYPE == 0.f) {
-//         outputColor = untonemapped;
-//       } else {
-//         outputColor = renodx::draw::ToneMapPass(untonemapped, tonemapped_bt709);
-//       }
-  return renodx::draw::RenderIntermediatePass(outputColor);
+    return renodx::color::srgb::EncodeSafe(outputColor);
+}
+
+float3 CustomPostProcessing(float3 color, float2 coords) {
+  float3 outputColor = renodx::color::srgb::DecodeSafe(color);
+  outputColor = renodx::effects::ApplyFilmGrain(outputColor, coords, CUSTOM_RANDOM, CUSTOM_FILM_GRAIN_STRENGTH * 0.03f);
+
+  return renodx::color::srgb::EncodeSafe(outputColor);
+}
+
+float3 CustomIntermediatePass(float3 color) {
+  return renodx::draw::RenderIntermediatePass(renodx::color::srgb::DecodeSafe(color));
 }
--- src/games/metro/shared.h
+++ src/games/metro/shared.h
@@ -5,9 +5,8 @@
 #define RENODX_DIFFUSE_WHITE_NITS            shader_injection.diffuse_white_nits
 #define RENODX_GRAPHICS_WHITE_NITS           shader_injection.graphics_white_nits
 #define RENODX_RENO_DRT_TONE_MAP_METHOD      renodx::tonemap::renodrt::config::tone_map_method::REINHARD
-#define RENODX_RENO_DRT_WHITE_CLIP           4.f
+#define RENODX_RENO_DRT_WHITE_CLIP           shader_injection.tone_map_white_clip
 #define RENODX_TONE_MAP_TYPE                 shader_injection.tone_map_type
-#define CUSTOM_TONE_MAP_CONFIGURATION        shader_injection.custom_tone_map_configuration
 #define RENODX_TONE_MAP_EXPOSURE             shader_injection.tone_map_exposure
 #define RENODX_TONE_MAP_HIGHLIGHTS           shader_injection.tone_map_highlights
 #define RENODX_TONE_MAP_SHADOWS              shader_injection.tone_map_shadows
@@ -18,11 +17,14 @@
 #define RENODX_TONE_MAP_FLARE                shader_injection.tone_map_flare
 #define RENODX_TONE_MAP_HUE_CORRECTION       shader_injection.tone_map_hue_correction
 #define RENODX_TONE_MAP_HUE_SHIFT            shader_injection.tone_map_hue_shift
-#define RENODX_TONE_MAP_WORKING_COLOR_SPACE  shader_injection.tone_map_working_color_space
-#define RENODX_TONE_MAP_HUE_PROCESSOR        shader_injection.tone_map_hue_processor
+#define RENODX_TONE_MAP_WORKING_COLOR_SPACE  1 //BT2020
+#define RENODX_TONE_MAP_HUE_PROCESSOR        1 //ICtCp
 #define RENODX_TONE_MAP_PER_CHANNEL          shader_injection.tone_map_per_channel
 #define RENODX_GAMMA_CORRECTION              shader_injection.gamma_correction
-#define CUSTOM_COLOR_GRADING                 shader_injection.custom_color_grading
+#define RENODX_COLOR_GRADE_STRENGTH          shader_injection.tone_map_color_grade_strength
+#define CUSTOM_COLOR_GRADE_TWO               shader_injection.custom_color_grade_two
+#define CUSTOM_FILM_GRAIN_STRENGTH           shader_injection.custom_film_grain_strength
+#define CUSTOM_RANDOM                        shader_injection.custom_random
 
 // Must be 32bit aligned
 // Should be 4x32
@@ -30,9 +32,8 @@ struct ShaderInjectData {
   float peak_white_nits;
   float diffuse_white_nits;
   float graphics_white_nits;
-  //float reno_drt_white_clip;
+  float tone_map_white_clip;
   float tone_map_type;
-  float custom_tone_map_configuration;
   float tone_map_exposure;
   float tone_map_highlights;
   float tone_map_shadows;
@@ -47,12 +48,16 @@ struct ShaderInjectData {
   float tone_map_hue_processor;
   float tone_map_per_channel;
   float gamma_correction;
-  float custom_color_grading;
+  float tone_map_color_grade_strength;
+  float custom_color_grading_two;
   float hue_correction_type;
+  float custom_color_grade_two;
+  float custom_film_grain_strength;
+  float custom_random;
 };
 
 #ifndef __cplusplus
-cbuffer cb13 : register(b13) {
+cbuffer cb11 : register(b11) {
   ShaderInjectData shader_injection : packoffset(c0);
 }
 
