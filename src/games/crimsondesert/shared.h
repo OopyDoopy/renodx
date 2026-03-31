2522bf3f3a224ad456009db4a57d8f55f1f78c7c
SGTForgery <golubeckasarnas@gmail.com>
2026-03-31 16:07:50 +0100

    feat(crimsondesert): Fixed Sun, Removed Alt Bloom + Fixed AMD Cbuffer crashing/black output issues, Removed all unused shaders since the update changed hashes

--- src/games/crimsondesert/shared.h
+++ src/games/crimsondesert/shared.h
@@ -25,13 +25,14 @@
 #define CUSTOM_CHROMATIC_ABERRATION            shader_injection.custom_chromatic_aberration
 #define CUSTOM_SHARPENING_TYPE                 shader_injection.custom_sharpening_type
 #define CUSTOM_SHARPENING                      shader_injection.custom_sharpening
-#define CUSTOM_VIGNETTE                       shader_injection.custom_vignette
+#define CUSTOM_VIGNETTE                        shader_injection.custom_vignette
 #define LENS_FLARE_STRENGTH                    shader_injection.lens_flare_strength
 #define BLOOM_STRENGTH                         shader_injection.bloom_strength
 
 #define SHADOW_DEBUG_MODE                      0 // shader_injection.shadow_debug_mode
 #define SHADOW_DISABLE_LAYER                   0 // shader_injection.shadow_disable_layer
 #define CONTACT_SHADOW_QUALITY                 shader_injection.contact_shadow_quality
+#define FOLIAGE_TRANSMISSION                   (CONTACT_SHADOW_QUALITY > 0.5f ? 1.0f : 0.0f)
 #define RT_QUALITY                             shader_injection.rt_quality
 #define MATERIAL_IMPROVEMENTS                  shader_injection.material_improvements
 #define DIFFUSE_BRDF_MODE                      (MATERIAL_IMPROVEMENTS > 0.5f ? 2.0f : 0.0f)
@@ -40,8 +41,8 @@
 #define DIFFRACTION                            (MATERIAL_IMPROVEMENTS > 0.5f ? 1.0f : 0.0f)
 #define LOCAL_LIGHT_HUE_CORRECTION             shader_injection.local_light_hue_correction
 #define LOCAL_LIGHT_SATURATION                 shader_injection.local_light_saturation
-#define DISABLE_AWB                            shader_injection.disable_awb
-#define DISABLE_HERO_LIGHTS                    shader_injection.disable_hero_lights
+#define DISABLE_AWB                            (shader_injection.disable_awb_mode > 0.5f ? 1.0f : 0.0f)
+#define DISABLE_HERO_LIGHTS                    (shader_injection.disable_awb_mode > 1.5f ? 1.0f : 0.0f)
 
 // Auto exposure tuning
 #define IMPROVED_AUTO_EXPOSURE                 shader_injection.improved_auto_exposure
@@ -52,21 +53,12 @@
 #define SUN_MOON_ADJUSTMENTS                   shader_injection.sun_moon_adjustments
 #define MOON_DISK_SIZE                         shader_injection.moon_disk_size
 #define SKY_SCATTERING                         shader_injection.sky_scattering
-#define FOLIAGE_TRANSMISSION                   (CONTACT_SHADOW_QUALITY > 0.5f ? 1.0f : 0.0f)
 
 // Auto exposure tuning
 #define AE_DARK_POWER_OUTDOOR                  shader_injection.ae_dark_power_outdoor
 #define AE_DYNAMISM                            shader_injection.ae_dynamism
 #define AE_SPEED                               shader_injection.ae_speed
 #define FOLIAGE_SHADOW_SENSITIVITY             0
-#define ALT_BLOOM                              shader_injection.alt_bloom    
-#define GLARE_SUN                              shader_injection.glare_sun        //1.0f
-#define GLARE_EMISSIVE                         shader_injection.glare_emissive   //0.5f
-#define GLARE_FOG                              shader_injection.glare_fog        //0.03f
-#define GLARE_PARTICLE26                       shader_injection.glare_particle26 //0.02f
-#define GLARE_PARTICLE27                       shader_injection.glare_particle27 //2.0f
-#define GLARE_NORMAL                           shader_injection.glare_normal
-#define GLARE_CLAMP                            shader_injection.glare_clamp      //20.0f
 #define AE_DARK_POWER_INDOOR                   0.55f
 #define AE_BRIGHT_POWER_OUTDOOR                1.00f
 #define AE_BRIGHT_POWER_INDOOR                 1.00f
@@ -84,7 +76,11 @@
 
 // Must be 32bit aligned
 // Should be 4x32
+//
 //// GAME BLOWS UP ONCE THERE'S MORE THAN 45 CBUFFERS ////
+// 
+//// AMD BLOWS UP AT 43-44 CBUFFERS BECASUE OF FSR    ////
+//
 struct ShaderInjectData {
   float peak_white_nits;
   float diffuse_white_nits;
@@ -116,8 +112,7 @@ struct ShaderInjectData {
   float material_improvements;
   float local_light_hue_correction;
   float local_light_saturation;
-  float disable_awb;
-  float disable_hero_lights;
+  float disable_awb_mode;
 
   float improved_auto_exposure;
   float ae_dark_power_outdoor;
@@ -129,14 +124,6 @@ struct ShaderInjectData {
   float sky_scattering;
   float lens_flare_strength;
   float bloom_strength;
-  float alt_bloom;
-  float glare_sun;
-  float glare_emissive;
-  float glare_fog;
-  float glare_particle26;
-  float glare_particle27;
-  float glare_normal;
-  float glare_clamp;
 };
 
 #ifndef __cplusplus
