34ee33b2ecc3fa61341f44656633c02377c82c36
OopyDoopy <jong833@gmail.com>
2026-04-10 11:44:17 -0400

    refactor(settings): rename presets to profiles

--- src/utils/settings.hpp
+++ src/utils/settings.hpp
@@ -398,8 +398,7 @@ static void SaveGlobalSettings() {
     }
   }
 
-  int saved_preset_index = (preset_index == 0 ? 1 : preset_index);
-  reshade::set_config_value(nullptr, global_name.c_str(), PROFILE_KEY, saved_preset_index);
+  reshade::set_config_value(nullptr, global_name.c_str(), PROFILE_KEY, preset_index);
 }
 
 static std::string ReadGlobalString(const std::string& key) {
