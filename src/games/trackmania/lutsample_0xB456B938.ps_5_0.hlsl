ed461bcd8af5f7d299287a659bca7c6729007860
OopyDoopy <jong833@gmail.com>
2026-06-18 14:49:42 -0400

    feat(trackmania): rework and overhaul for mod, still WIP

--- src/games/trackmania/lutsample_0xB456B938.ps_5_0.hlsl
+++ src/games/trackmania/lutsample_0xB456B938.ps_5_0.hlsl
@@ -1,4 +1,4 @@
-#include "./shared.h"
+#include "./common.hlsl"
 
 // ---- Created with 3Dmigoto v1.4.1 on Sat Apr 12 01:37:46 2025
 
@@ -36,13 +36,20 @@ void main(
 
   r0.xyzw = TMapInput.Sample(SGbxClamp_Point_s, v1.xy).xyzw;
 
-  r0.xyz = renodx::draw::InvertIntermediatePass(r0.xyz);
+  r0.xyz = InvertIntermediatePass(r0.xyz);
+
+  float3 ungraded = r0.xyz;
+  SceneGradeGamutCompressionState gamut_compression_state = SetupSceneGradeGamutCompression(r0.xyz);
 
   r1.xyz = g_CBuffer.TcScale.xyz * r0.xyz + g_CBuffer.TcTrans.xyz;
   r1.xyz = TMapRemap.Sample(SGbxClamp_Trilinear_s, r1.xyz).xyz;
   o0.xyz = g_CBuffer.wIdentity * r0.xyz + r1.xyz;
+
+  o0.xyz = InvertSceneGradeGamutCompression(o0.xyz, gamut_compression_state);
+  o0.xyz = lerp(ungraded, o0.xyz, SCENE_GRADE_COLOR_GRADE_STRENGTH_TWO);
+
   o0.w = r0.w;
 
-  o0.rgb = renodx::draw::RenderIntermediatePass(o0.rgb);
+  o0.rgb = IntermediatePass(o0.rgb);
   return;
 }
